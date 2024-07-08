e64b19404fad33247a2436e8685e6da84c485986
Raphael da Silva <119632711+RaphaLsantos@users.noreply.github.com>
2024-07-08 15:16:53 -0300

    home
    home
    
    Co-Authored-By: jogbiel <165044823+jogbiel@users.noreply.github.com>

--- user/js/js.js
+++ user/js/js.js
@@ -22,3 +22,29 @@ document.addEventListener("DOMContentLoaded", function() {
     setInterval(rotateImage, 4000);
 });
 //efeito rotação automática da imagem final
+
+
+//navbar ao descer a tela
+const navBar = document.querySelector('.nav_bar');
+let lastScrollTop = 0;
+const delta = 5; 
+
+window.addEventListener('scroll', () => {
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (Math.abs(lastScrollTop - scrollTop) <= delta)
+        return; 
+
+    if (scrollTop > lastScrollTop) {
+        // Scroll para baixo
+        navBar.classList.add('hidden');
+    } else {
+        // Scroll para cima
+        navBar.classList.remove('hidden');
+    }
+    
+    lastScrollTop = scrollTop;
+});
+
+
+//navbar ao descer a tela
