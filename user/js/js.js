c026ed905412724e2a01ea573b3159c1987bb890
jogbiel <jogbiel69@gmail.com>
2024-07-09 15:20:08 -0300

    Update js.js

--- user/js/js.js
+++ user/js/js.js
@@ -66,3 +66,42 @@ document.getElementById('startInicio').addEventListener('click', function() {
 
 
 //fim efeito voltar ao inicio
+
+// inicio carrossel galeria
+const track = document.querySelector('.carousel-track');
+        const slides = Array.from(track.children);
+        const nextButton = document.querySelector('.next-btn');
+        const prevButton = document.querySelector('.prev-btn');
+        const slideWidth = slides[0].getBoundingClientRect().width;
+
+        // Arrange the slides next to one another
+        const setSlidePosition = (slide, index) => {
+            slide.style.left = slideWidth * index + 'px';
+        };
+        slides.forEach(setSlidePosition);
+
+        const moveToSlide = (track, currentSlide, targetSlide) => {
+            track.style.transform = 'translateX(-' + targetSlide.style.left + ')';
+            currentSlide.classList.remove('current-slide');
+            targetSlide.classList.add('current-slide');
+        };
+
+        // Click right, move slides to the right
+        nextButton.addEventListener('click', e => {
+            const currentSlide = track.querySelector('.current-slide');
+            let nextSlide = currentSlide.nextElementSibling;
+            if (!nextSlide) {
+                nextSlide = slides[0];
+            }
+            moveToSlide(track, currentSlide, nextSlide);
+        });
+
+        // Click left, move slides to the left
+        prevButton.addEventListener('click', e => {
+            const currentSlide = track.querySelector('.current-slide');
+            let prevSlide = currentSlide.previousElementSibling;
+            if (!prevSlide) {
+                prevSlide = slides[slides.length - 1];
+            }
+            moveToSlide(track, currentSlide, prevSlide);
+        });
